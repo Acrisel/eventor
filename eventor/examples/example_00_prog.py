9bd2e230319e295d8a99329087d683ea3cc88de4
Arnon Sela <arnon@acrisel.com>
2017-09-02 16:40:23 -0500

    remove dill

--- eventor/examples/example_00_prog.py
+++ eventor/examples/example_00_prog.py
@@ -4,7 +4,7 @@ Created on Sep 2, 2017
 @author: arnon
 '''
 
-def prog(progname):
+def prog(progname, logger):
     logger.info("doing what %s is doing" % progname)
     logger.info("EVENTOR_STEP_SEQUENCE: %s" % os.getenv("EVENTOR_STEP_SEQUENCE"))
     return progname
